2e39ea964fc955d548c650fd5b24b613f1a188fb
vivekgurung00000-svg <vivekgurung00000@gmail.com>
2026-02-14 23:09:45 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,29 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("birthday-cache").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "style.css",
-        "script.js",
-        "manifest.json"
-      ]);
-    })
+const CACHE_NAME = "love-page-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./music.mp3",
+  "./photo1.jpg",
+  "./photo2.jpg",
+  "./photo3.jpg",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
